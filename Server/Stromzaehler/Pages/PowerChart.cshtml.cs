09a63b53d94af9cf39bba58987f3653157484399
Boris Breidenbach <boris.breidenbach@bayernlb.de>
2019-07-17 20:47:10 +0200

    Add power chart

---
+++ Server/Stromzaehler/Pages/PowerChart.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Stromzaehler.Models;
+using Stromzaehler.Tools;
+
+namespace Stromzaehler.Pages
+{
+    public class PowerChartModel : PageModel
+    {
+        private List<Blink> blinks;
+        private readonly BlinkDataContext db;
+
+        public PowerChartModel(BlinkDataContext db)
+        {
+            this.db = db ?? throw new System.ArgumentNullException(nameof(db));
+        }
+
+        public async Task OnGetAsync()
+        {
+            blinks = await db.Blinks.AsNoTracking().ToListAsync();
+        }
+
+        public string GetLabels()
+        {
+            return $"['{string.Join("','", blinks.Select(b => b.Timestamp.ToString()).ToArray())}']";
+        }
+
+        public string GetData()
+        {
+            var values = blinks
+                .Diff((next, prev) => 
+                3600 * (next.Value - prev.Value) / (prev.Timestamp - next.Timestamp).TotalSeconds);
+            return $"[{string.Join(',', values)}]";
+        }
+    }
+}
---
+++ Server/Stromzaehler/Tools/EnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stromzaehler.Tools
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<TResult> Diff<TSource, TResult>
+        (this IEnumerable<TSource> source,
+         Func<TSource, TSource, TResult> projection)
+        {
+            using (var iterator = source.GetEnumerator())
+            {
+                if (!iterator.MoveNext())
+                {
+                    yield break;
+                }
+                TSource previous = iterator.Current;
+                while (iterator.MoveNext())
+                {
+                    yield return projection(previous, iterator.Current);
+                    previous = iterator.Current;
+                }
+            }
+        }
+    }
+}
