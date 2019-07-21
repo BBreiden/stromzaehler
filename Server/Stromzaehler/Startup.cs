0f1b3dd43c4d0b4cd7f62697e429be07226c6f5d
Boris Breidenbach <bo@chrbo.net>
2019-07-21 20:41:30 +0200

    Configuration for database path

--- Server/Stromzaehler/Startup.cs
+++ Server/Stromzaehler/Startup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -5,18 +7,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Stromzaehler.Models;
 
 namespace Stromzaehler
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, ILogger<Startup> log)
         {
             Configuration = configuration;
+            Log = log;
         }
 
         public IConfiguration Configuration { get; }
+        public ILogger<Startup> Log { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,7 +36,16 @@ namespace Stromzaehler
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             //services.AddDbContext<BlinkDataContext>(options => options.UseInMemoryDatabase());
-            services.AddDbContext<BlinkDataContext>(o => o.UseSqlite("Filename=BlinkData.db"));
+            var dbLocation = Configuration.GetSection("Database:Path").Value;
+            if (string.IsNullOrEmpty(dbLocation)) {
+                throw new InvalidOperationException("Missing database path.");
+            }
+            if (!Directory.Exists(dbLocation)) {
+                throw new InvalidOperationException($"Directory does not exist: {dbLocation}");
+            }
+            var dbPath = Path.Combine(dbLocation, "BlinkData.db");
+            Log.LogInformation($"Using database {dbPath}");
+            services.AddDbContext<BlinkDataContext>(o => o.UseSqlite($"Filename=BlinkData.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
